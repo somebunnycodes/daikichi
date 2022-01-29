3fb555892d911c3fce2ec28bb6b4fa17d657f13f
Bunny Mueller <thebunnymueller@gmail.com>
2022-01-30 00:08:20 +0100

    Assignment part 2

--- src/main/java/com/somebunnycodes/daikichi/DaikichiController.java
+++ src/main/java/com/somebunnycodes/daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.somebunnycodes.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,19 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String herald() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel (@PathVariable String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	
+	@RequestMapping("/lotto/{lottoNumber}")
+	public String lotto (@PathVariable int lottoNumber) {
+		if (lottoNumber %2==0) { 
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		} else { 
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
