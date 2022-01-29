e9166157ee1b1e2e53d043e06fcdcc17000669cc
Bunny Mueller <thebunnymueller@gmail.com>
2022-01-29 15:20:56 +0100

    Initial commit

---
+++ src/main/java/com/somebunnycodes/daikichi/DaikichiController.java
@@ -0,0 +1,24 @@
+package com.somebunnycodes.daikichi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	
+	@RequestMapping("/")
+	public String welcome() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavours!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String herald() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+}
